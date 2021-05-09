11b0e91a1f0107984bb49856da54eae18def65ab
dracielisawal <awal.singh04@gmail.com>
2021-05-09 16:46:32 +0530

    Imported Assetpack (lowpoly)

---
+++ Assets/Scripts/MenuOpener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuOpener : MonoBehaviour
+{
+    public Image objMenu;
+    private bool _isMenuOpen;
+   public void onbuttonclick()
+    {
+        if (_isMenuOpen)
+            openMenu();
+
+
+    } 
+    void openMenu()
+    {
+        
+    }
+}
