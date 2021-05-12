77751eec146d7ef913b79f2b5a2ea01ab85110e7
dracielisawal <awal.singh04@gmail.com>
2021-05-12 20:19:38 +0530

    Simple Pause menu UI created

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+   public GameObject pausemenu;
+    private bool _isPauseMenuOpen;
+    
+   public void pauseMenuopener()
+    {
+        if (_isPauseMenuOpen == false)
+        {
+            pausemenu.SetActive(true);
+            _isPauseMenuOpen = true;
+            return;
+        }
+        else
+        {
+            pausemenu.SetActive(false);
+            _isPauseMenuOpen = false;
+        }
+    }
+}
