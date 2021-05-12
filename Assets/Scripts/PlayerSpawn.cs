dfe73c812284f5c81b2a01bbdcb39f59cb0b99ac
dracielisawal <awal.singh04@gmail.com>
2021-05-12 19:48:23 +0530

    Edit map after spawning functionality button

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class PlayerSpawn : MonoBehaviour
 {
     public GameObject player;
+    public GameObject editButton;
     public GameObject worldcreatorui;
     public GameObject objectselector;
    
     
  public void oncreationDone()
     {
+        editButton.SetActive(true);
         PlacerObj.instance.activeObject = null;
         player.SetActive(true);
         worldcreatorui.SetActive(false);
         objectselector.SetActive(false);
         
+        
+    } 
+
+    public void Editmap()
+    {
+        editButton.SetActive(false);
+        player.SetActive(false);
+        worldcreatorui.SetActive(true);
+        objectselector.SetActive(true);
+
     }
-   
+
 
 }
